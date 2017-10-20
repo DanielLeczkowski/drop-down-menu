ad3789e24e3e6b16457005a88e6703293193c74a
DanielLeczkowski <daniel.leczkowski@wp.pl>
2017-10-20 03:05:45 +0200

    add click circle function

--- js/js.js
+++ js/js.js
@@ -3,4 +3,80 @@ $(document).ready(function(){
     let navMenu = $('.nav-menu');
     let subMenuContainer = $('.submenu');
     let arrowDown = $('.i_arrow');
+
+        $('body').click(function(event){
+        let x = event.clientX;
+        let y = event.clientY;
+        console.log(x, y);
+        $('body').append('<div class="circle" style="left:' + x + 'px; top:' + y + 'px;"></div>');
+        setTimeout(function(){
+            $('div.circle').remove();
+        },50);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
