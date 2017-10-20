7c2b37495c51203b632c6fb91942ae575cf60d32
DanielLeczkowski <daniel.leczkowski@wp.pl>
2017-10-20 03:07:13 +0200

    add navMenu background color

--- js/js.js
+++ js/js.js
@@ -14,6 +14,46 @@ $(document).ready(function(){
         },50);
     });
 
+    navMenu.click(function(){
+    
+        let $this = $(this);
+        
+        $this.toggleClass('open');
+        
+        if($this.hasClass('open')){
+                mainContainer.append(`
+                    <div class="triangle"></div>
+                    <div class="submenu">
+                            <div class="submenu-items">
+                                <i class="i_home"></i>
+                                <a class="dropdown">Home</a>
+                            </div>
+                            <div class="submenu-items">
+                                <i class="i_mail"></i>
+                                <a class="dropdown">Messages</a>
+                            </div>
+                            <div class="submenu-items">
+                                <i class="i_cog"></i>
+                                <a class="dropdown">Settings</a>
+                            </div>
+                    </div>
+                `);
+                    navMenu.css('background-color', '#3ea6fd');
+                    $('.nav-menu > a').css('color', 'rgba(255, 255, 255, 0.9)');
+                    $('.nav-menu > i').css('color', 'rgba(255, 255, 255, 0.9)');
+                    arrowDown.css('transform', 'rotate(-180deg)');
+                    
+        } else {
+                    $this.removeClass('open');
+                    navMenu.css('background-color', 'rgba(255, 255, 255, 0.9)');
+                    $('.nav-menu > a').css('color', '#7d7988');
+                    $('.nav-menu > i').css('color', '#7d7988');
+                    $('div.submenu').remove();
+                    $('div.triangle').remove();
+                    arrowDown.css('transform', 'rotate(0deg)');
+        }
+    });
+
 
 
 
